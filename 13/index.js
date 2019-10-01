3b66bee335f352ca0bed31457ade3bf0df1e4f52
panhaiwei <panhaiwei812@qq.com>
2019-10-01 15:51:45 +0800

    add notes

--- 13/index.js
+++ 13/index.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 var romanToInt = function (s) {
+    // 添加映射
     const _hashMap = {
         "I": 1,
         "V": 5,
@@ -14,10 +15,10 @@ var romanToInt = function (s) {
     }
     let _number = 0;
     if (s) {
-        const _str = s.split("");
+        const _str = s.split("");// 分割字符串计算
         for (let index = 0; index < _str.length; index++) {
             const element = _str[index];
-            if (index < _str.length && (_hashMap[element]) < _hashMap[_str[index + 1]]) {
+            if (index < _str.length && (_hashMap[element]) < _hashMap[_str[index + 1]]) {// 判断右边是否大于当前
                 _number -= _hashMap[element];
             } else {
                 _number += _hashMap[element];
