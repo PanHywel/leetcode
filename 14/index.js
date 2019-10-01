75967bfdc6ea7af82d76d74d98b95c0473716e2b
panhaiwei <panhaiwei812@qq.com>
2019-10-01 17:31:33 +0800

    add 14

--- 14/index.js
+++ 14/index.js
@@ -2,6 +2,27 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-    
+var longestCommonPrefix = function (strs) {
+    let length = 0;
+    let result = ""
+    for (let i = 0; i < strs.length; i++) { // 查找数组中最短的字符
+        length = strs[i].length
+        if (length === 0) {
+            length = strs[i].length;
+        } else {
+            if (strs[i].length < length) {
+                length = strs[i].length;
+            }
+        }
+    }
+    for (let j = 0; j < length; j++) {
+        let s = strs[0][j]
+        if (strs.every(val => val[j] == s)) {
+            result += s
+        } else {
+            break
+        }
+    }
+    return result
 };
+longestCommonPrefix(["flower", "flow", "flight"])
