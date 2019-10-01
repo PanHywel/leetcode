ba70794998d23ec28e418fb73c3d694af6420a2c
panhaiwei <panhaiwei812@qq.com>
2019-10-01 15:42:34 +0800

    add content

--- 13/index.js
+++ 13/index.js
@@ -3,28 +3,24 @@
  * @return {number}
  */
 var romanToInt = function (s) {
-    var hashMap = {
-        Set: function (key, value) { this[key] = value },
-        Get: function (key) { return this[key] },
-        Contains: function (key) { return this.Get(key) == null ? false : true },
-        Remove: function (key) { delete this[key] }
-    };
-    hashMap.Set("I", 1);
-    hashMap.Set("V", 5);
-    hashMap.Set("X", 10);
-    hashMap.Set("L", 50);
-    hashMap.Set("C", 100);
-    hashMap.Set("D", 500);
-    hashMap.Set("M", 1000);
+    const _hashMap = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
     let _number = 0;
     if (s) {
         const _str = s.split("");
         for (let index = 0; index < _str.length; index++) {
             const element = _str[index];
-            if (index < _str.length && (hashMap.Get(element)) < hashMap.Get(_str[index + 1])) {
-                _number -= hashMap.Get(element);
+            if (index < _str.length && (_hashMap[element]) < _hashMap[_str[index + 1]]) {
+                _number -= _hashMap[element];
             } else {
-                _number += hashMap.Get(element);
+                _number += _hashMap[element];
             }
         }
     }
